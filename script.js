cc99d3adfe3da13ba2d81c79161a501d211d6285
Alexey Shulzhenko <lexa270@gmail.com>
2016-05-17 01:30:16 +0300

    Bug fixes

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var numProd = 1;
+var x = 1;
 var firstPart = ('<div class="blockM prodLine" style="display: block;"><div class="lable"><span class="prodName">');
-var secondPart = ('</span></div><div class="calcButtons"><button class="addButton minusButton" disabled="true"><img src="http://www.veryicon.com/icon/32/System/iOS7%20Minimal/Basic%20Minus.png"/></button> <span class="numOfProd">1</span> <button class="addButton plusButton"><img src="http://icons.iconarchive.com/icons/icons8/ios7/32/User-Interface-Plus-icon.png"/></button></div> <div class="spareButtons">                    <button class="buy-button">Куплено</button>                      <button class="deleteBttn" title="Видалити">                            <i>X</i>                       </button>               </div>               </div> ');
+var secondPart = ('</span></div><div class="calcButtons"><button class="addButton minusButton" disabled="true"><img src="http://www.veryicon.com/icon/32/System/iOS7%20Minimal/Basic%20Minus.png"/></button> <span class="numOfProd">' + numProd + '</span> <button class="addButton plusButton"><img src="http://icons.iconarchive.com/icons/icons8/ios7/32/User-Interface-Plus-icon.png"/></button></div> <div class="spareButtons">                    <button class="buy-button">Куплено</button>                      <button class="deleteBttn" title="Видалити">                            <i>X</i>                       </button>               </div>               </div> ');
 $(document).ready(function(){
+    
     $(".buttonAdder").click(function(){
                 $(".leftBox").append(firstPart + $('#productName').val() + secondPart);
+                $(".balance").append('<span class="numOfProd"><span class="title">' +  $('#productName').val() +' </span>  <span class="miniOrangeCircle label count">' + numProd + '</span></span>');
         });
+    
+    $(".plusButton").click(function() {
+        numProd++;
+                $(".numOfProd").text(numProd);
+        console.log("numProd", numProd);
+    });
 });
