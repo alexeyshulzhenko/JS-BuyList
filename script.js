0593f9083bf4dd5ba82583dca934975d145cf580
Alexey Shulzhenko <lexa270@gmail.com>
2016-05-18 10:36:19 +0300

    Done!!!

--- script.js
+++ script.js
@@ -2,33 +2,33 @@
 $(function(){
     function createNewElement(title){
         var node = $('<div class="blockM prodLine" style="display: block;">'+
-            '<div class="lable">'+
-                '<span class="prodName">' + title + '</span></div>'+
-            '<div class="calcButtons">'+
+                '<div class="lable">'+
+                '<span class="prodName"><input type="text" class="editer"  value="' + title + '"></span></div>'+
+                '<div class="calcButtons">'+
                 '<button class="addButton minusButton" disabled="">'+
                 '<img src="http://www.veryicon.com/icon/32/System/iOS7%20Minimal/Basic%20Minus.png"/>'+
                 ' </button> '+
                 '<span class="numOfProd">1</span> '+
                 '<button class="addButton plusButton">'+
-               '      <img src="http://icons.iconarchive.com/icons/icons8/ios7/32/User-Interface-Plus-icon.png"/>'+
-               ' </button>'+
-               ' </div>'+
-               ' <div class="spareButtons">'+
-               '  <button class="buy-button buy">Куплено</button>'+
-               ' <button hidden="hidden" class="buy-button not-buy" >Не куплено</button>'+
-               ' <button class="deleteBttn" title="Видалити">'+
-               '     <i>X</i>'+
-               ' </button></div> </div>'
+                '      <img src="http://icons.iconarchive.com/icons/icons8/ios7/32/User-Interface-Plus-icon.png"/>'+
+                ' </button>'+
+                ' </div>'+
+                ' <div class="spareButtons">'+
+                '  <button class="buy-button buy">Куплено</button>'+
+                ' <button  class="buy-button not-buy" >Не куплено</button>'+
+                ' <button class="deleteBttn" title="Видалити">'+
+                '     <i>X</i>'+
+                ' </button></div> </div>'
         );
         
         var node2 =$( '<span class="numOfProd">'+'<span class="title">'+title+'</span> '+
-            '<span class="miniOrangeCircle label count">1</span>'
-        +'</span>'
+            '<span class="miniOrangeCircle label count">1</span>' +
+            '</span>'
         );
         
-        var node3 =$( '<span class="numOfProd">'+'<span class="title">'+title+'</span> '+
-            '<span class="miniOrangeCircle label count">1</span>'
-        +'</span>'
+        var node3 =$( '<span class="numOfProd bougtnList">'+'<span class="title">'+title+'</span> '+
+            '<span class="miniOrangeCircle label count">1</span>' +
+            '</span>'
         );
   
             console.log("done!");
@@ -39,40 +39,41 @@ $(function(){
             node.find('.calcButtons').hide();
             node.find('.not-buy').show();
             node.find('.deleteBttn').hide();
+            node.find('.editer').attr('disabled',true);
             node2.hide();
             node3.show();
         });
         
         node.find('.plusButton').click(function(){
-            var old=parseInt(node.find(".numOfProd").text(),10);
+            var coun=parseInt(node.find(".numOfProd").text(),10);
 
-            if(old>=1){
-                node.find(".minusButton").removeClass("but-block");
+            if(coun>=1){
+                node.find(".minusButton").attr('disabled',false);
             }
-            if(old<30){
-                old++;
+            if(coun<30){
+                coun++;
             }
 
-            node.find(".numOfProd").text(old);
-            node2.find(".count").text(old);
-            node3.find(".count").text(old);
+            node.find(".numOfProd").text(coun);
+            node2.find(".count").text(coun);
+            node3.find(".count").text(coun);
 
         });
 
-        node.find('.inusButton').click(function(){
-            var old=parseInt(node.find(".numOfProd").text(),10);
+        node.find('.minusButton').click(function(){
+            var coun=parseInt(node.find(".numOfProd").text(),10);
 
-            if(old<2){
-                node.find(".inusButton").addClass("but-block");
-                old=1;
+            if(coun<2){
+                node.find(".minusButton").attr('disabled',true);
+                coun=1;
             }
 
-            if((old<=30)&&(old>1)){
-                old--;
+            if((coun<=30)&&(coun>1)){
+                coun--;
             }
-            node.find(".numOfProd").text(old);
-            node2.find(".count").text(old);
-            node3.find(".count").text(old);
+            node.find(".numOfProd").text(coun);
+            node2.find(".count").text(coun);
+            node3.find(".count").text(coun);
         });
 
 
@@ -110,9 +111,9 @@ $(function(){
         }
     });
     
-    createNewElement("Смородинка");
-    createNewElement("Полуничка");
-    createNewElement("Шоколадка");
+    createNewElement("Помідори");
+    createNewElement("Печиво");
+    createNewElement("Сир");
 
  }); 
 
